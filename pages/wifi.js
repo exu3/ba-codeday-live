b84f53dc35588da794da95be4f8d9b58d821a358
eilla1 <git@ella.cx>
2021-11-13 11:06:02 -0800

    let there be wifi

---
+++ pages/wifi.js
@@ -0,0 +1,15 @@
+export default function Wifi() {
+  return (
+    <main>
+      <h1>WiFi Password</h1>
+      <p> SSID: ATT6H4d3w5</p>
+      <p> Password: 7v2z4h3k942i </p>
+
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src="https://cloud-v6llxbj5i-hack-club-bot.vercel.app/0screen_shot_2021-11-13_at_11.01.11_am.png"
+        alt="qr code"
+      />
+    </main>
+  );
+}
